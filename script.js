5bde03f398b5497e7ed0931c3c0d0405faf843ba
David Lu <conundrumer@gmail.com>
2014-06-27 02:16:53 -0400

    html and js boilerplate

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var canvas = document.querySelector("canvas");
+    var ctx = canvas.getContext("2d");
+    var audio = new AudioContext();
+    console.log(ctx, audio);
+};
