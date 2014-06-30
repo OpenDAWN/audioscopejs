aebac514aff96014fb48c29eff4f6e9f81a2d080
David Lu <davidelu93@gmail.com>
2014-06-30 00:59:55 -0400

    enumerated channel numbers

---
+++ js/audio/quadChan.js
@@ -0,0 +1,13 @@
+/**
+ * Enumeration of channel numbers
+ */
+define(function() {
+	return {
+		L: 0, // left
+		R: 1, // right
+		LS: 2, // left shifted
+		RS: 3, // right shifted
+		LR: 2, // number of channels in stereo
+		QUAD: 4 // number of channels in quadrature
+	};
+});
--- js/audio/quadProc.js
+++ js/audio/quadProc.js
@@ -2,28 +2,21 @@
  * Quadrature processor node: takes stereo in
  * and gives time domain out with 90˚ phase shift pairs
  */
-define(['audio/hilbert'], function(HilbertNode) {
-	var L = 0,
-		R = 1,
-		Ls = 2, // phase shifted 90 degrees channels
-		Rs = 3,
-		LR = 2,
-		QUAD = 4; // quadruple/quadratureLR
+define(['audio/hilbert','audio/quadChan'], function(HilbertNode, C) {
 
 	function QuadProcessor(audio, bufLength, onProcess, fs) {
-
 		var hilbert = new HilbertNode(audio, fs);
 		// apparently it gets gc'd if not referenced...?
-		this.processor = audio.createScriptProcessor(bufLength, QUAD, 0);
-		var inputSplitter = audio.createChannelSplitter(LR);
-		var hilbertSplitter = audio.createChannelSplitter(LR);
-		var inputMerger = audio.createChannelMerger(QUAD);
+		this.processor = audio.createScriptProcessor(bufLength, C.QUAD, 0);
+		var inputSplitter = audio.createChannelSplitter(C.LR);
+		var hilbertSplitter = audio.createChannelSplitter(C.LR);
+		var inputMerger = audio.createChannelMerger(C.QUAD);
 
 		hilbert.connect(hilbertSplitter);
-		inputSplitter.connect(inputMerger, L, L);
-		inputSplitter.connect(inputMerger, R, R);
-		hilbertSplitter.connect(inputMerger, L, Ls);
-		hilbertSplitter.connect(inputMerger, R, Rs);
+		inputSplitter.connect(inputMerger, C.L, C.L);
+		inputSplitter.connect(inputMerger, C.R, C.R);
+		hilbertSplitter.connect(inputMerger, C.L, C.Ls);
+		hilbertSplitter.connect(inputMerger, C.R, C.Rs);
 		inputMerger.connect(processor);
 
 		this.connectToSource = function(source) {
