845e198b544c358db9f603f76e525fc42b07c1bf
David Lu <davidelu93@gmail.com>
2014-06-30 02:24:25 -0400

    add window functions

---
+++ js/audio/windows.js
@@ -0,0 +1,43 @@
+/**
+ * Cosine windows
+ */
+define(function() {
+	function Window(name, a, length) {
+		var fn = makeWindowFn(a);
+		var w = new Float32Array(length);
+		for (var n = 0; n < length; n++) {
+			w[n] = fn(n);
+		}
+		return {
+			name: name,
+			fn: w
+		};
+	}
+
+	Window.prototype.doWindow = function doWindow(output, input) {
+		for (var n = 0; n < input.length; n++) {
+			output[n] = input[n] * this.fn[n];
+		}
+	};
+
+	function makeWindowFn(coeffs) {
+		return function(n, N) {
+			coeffs.map(function(a, k) {
+				return a * Math.cos(2 * Math.PI * k * n / (N - 1));
+			}).reduce(function(x, y) {
+				return x + y;
+			});
+		};
+	}
+
+	function makeWindow(name, a) {
+		return Window.bind(null, name, a);
+	}
+
+	return {
+		Rect: makeWindow('rect', [1]),
+		Hamming: makeWindow('hamming', [25/46, -21/46]),
+		Blackman: makeWindow('blackman', [7938/18608, -9240/18608, 1430/18608]),
+		BlackmanHarris: makeWindow('blackmanharris', [0.35875, -0.48829, 0.14128, -0.01168])
+	};
+});
