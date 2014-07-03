0b068f9c76a4acd1501c8ba1803e4413e8d647cf
David Lu <davidelu93@gmail.com>
2014-07-03 02:21:24 -0400

    analyzer: specification + make into class + add init/set functions

--- js/audio/analyzer.js
+++ js/audio/analyzer.js
@@ -1,16 +1,52 @@
-define(['audio/input', 'audio/quadBuffer'], function(input, QuafBuffer) {
-	var PROCESSOR_BUFFER_SIZE = 256;
+/**
+ * Takes stereo (with 90˚ phase shift pair for converting to analytic)
+ * and outputs frames of various representations of the signals:
+ ** Signal combinations **
+ * Left / Right
+ * Mid / Side
+ ** Time Domain **
+ * Mono amplitude
+ ** Frequency Domain **
+ * Mono magnitude
+ * Stereo phase difference + Total magnitude + Panning
+ ** Analytic **
+ * Mono Real + Imaginary amplitude
+ * Mono Magnitude + Phase
+ * Mono Magnitude + (instantaneous) Frequency
+ * Stereo phase difference + Total magnitude + Panning
+ */
+define(['audio/quadBuffer'], function(QuadBuffer) {
+	var BUFFER_LENGTH = 1.0; // seconds
+	var PROCESSOR_STEP_SIZE = 256;
 
-	var Analyzer = {};
-	var audio;
+	function Analyzer(audio) {
+		var fs = audio.sampleRate;
+		this.audio = audio;
+		this.buffer = new QuadBuffer(audio, BUFFER_LENGTH * fs, PROCESSOR_STEP_SIZE, fs);
+	}
 
-	Analyzer.init = function() {
-		audio = new AudioContext();
-		// var buffer = new Buffer(audio, PROCESSOR_BUFFER_SIZE);
-
-		input.getUserMedia(function(inputNode) {
-			// buffer.connectToSource(inputNode);
-		});
+	Analyzer.prototype = {
+		/* functions that should be called to further initialize */
+		/**
+		 * Takes a node producing audio output for analysis
+		 */
+		setInput: function(input) {
+			this.buffer.processor.connectToSource(inputNode);
+		},
+		/**
+		 * Sets the delay (in seconds)
+		 * More delay makes more accurate analytic signals
+		 */
+		setDelay: function(delay) {
+			this.buffer.processor.setDelay(delay * this.audio.fs);
+		},
+		/**
+		 * Sets how frequently frames will be obtained
+		 */
+		setFps: function(fps) {
+			this.stepSize = Math.floor(this.audio.sampleRate / fps);
+		}
 	};
+
 	return Analyzer;
 });
