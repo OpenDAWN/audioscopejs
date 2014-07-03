29cad686af2ae283dd63727f649de7230a50335f
David Lu <davidelu93@gmail.com>
2014-07-03 00:50:52 -0400

    HilbertNode: Add node for unphased delayed signal

--- js/audio/hilbert.js
+++ js/audio/hilbert.js
@@ -1,36 +1,58 @@
+/**
+ * Processes stereo signals and outputs 90˚phase shifted signal
+ * and a properly delayed unphase-shifted signal
+ */
 define(['audio/windows', 'audio/quadChan'], function(w, C) {
 
 	function HilbertNode(audio, fs) {
 		this.createBuffer = function(N) {
 			return audio.createBuffer(C.LR, N, fs);
 		};
-		this.node = audio.createConvolver();
-		this.node.normalize = false;
+		// delay node has additional smoothing processing so do this instead
+		this.unphasedNode = audio.createConvolver();
+		this.phasedNode = audio.createConvolver();
+		this.unphasedNode.normalize = false;
+		this.phasedNode.normalize = false;
 	}
+	/* don't call this too often, making buffers/arrays take a while */
 	HilbertNode.prototype.setFilterLength = function(N) {
+		if (N < 2) {
+			this.setNoFilter();
+			return;
+		}
 		// N should be odd length
-		var M = Math.floor(N/2);
-		N = M*2 + 1;
-		var hamming = new w.Hamming(N);
-		var buf = this.createBuffer(N);
-		var left = buf.getChannelData(C.L),
-		right = buf.getChannelData(C.R);
+		var M = Math.floor(N / 2);
+		N = M * 2 + 1;
+
+		// delay unphased
+		var delayIR = this.createBuffer(M);
+		delayIR.getChannelData(C.L)[M-1] = 1;
+		delayIR.getChannelData(C.R)[M-1] = 1;
+		this.unphasedNode.buffer = delayIR;
+
+		// generate phased IR
+		var hilbertIR = this.createBuffer(N);
+		var left = hilbertIR.getChannelData(C.L),
+			right = hilbertIR.getChannelData(C.R);
 		for (var i = 0; i < N; i++) {
 			var n = i - M;
-			var f_n = 2 / (Math.PI * n) * Math.abs(n%2);
+			var f_n = 2 / (Math.PI * n) * Math.abs(n % 2);
 			left[i] = f_n;
 			right[i] = f_n;
-			console.log(i, n, f_n);
 		}
 		left[M] = 0;
 		right[M] = 0;
-		console.log(hamming);
+		var hamming = new w.Hamming(N);
 		hamming.doWindow(left, left);
 		hamming.doWindow(right, right);
-		console.log(left);
-		console.log(buf);
-		this.node.buffer = buf;
+		this.phasedNode.buffer = hilbertIR;
 	};
-	window.H = HilbertNode;
+	HilbertNode.prototype.setNoFilter = function() {
+		var buf = this.createBuffer(1);
+		buf.getChannelData(C.L)[0] = 1;
+		buf.getChannelData(C.R)[0] = 1;
+		this.unphasedNode.buffer = buf;
+		this.phasedNode.buffer = buf;
+	};
 	return HilbertNode;
 });
